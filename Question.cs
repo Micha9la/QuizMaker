d51b71d0fff47c16530cae489809790c6b896a7d
michaela <m.hunarwindhager@gmail.com>
2025-04-10 14:16:35 +0200

    Add project files.

---
+++ Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker
+{
+    [Serializable]
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public List<string> PossibleAnswers { get; set; }
+        public List<int> CorrectAnswerIndices { get; set; }
+
+        public Question()
+        {
+            PossibleAnswers = new List<string>();
+            CorrectAnswerIndices = new List<int>();
+        }
+    }
+}
