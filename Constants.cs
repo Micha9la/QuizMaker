97be4ca9e46497b22f189fa12733002dd7e8dfa5
michaela <m.hunarwindhager@gmail.com>
2025-04-11 15:32:25 +0200

    Created a new Class QnA and created related UI Methods to ask user for questions, answer count, answers, correct answers and saved it in a list.

--- Constants.cs
+++ Constants.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace QuizMaker
 {
-    internal class Constants
+    public class Constants
     {
     }
 }
--- Logic.cs
+++ Logic.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace QuizMaker
 {
-    internal class Logic
+    public class Logic
     {
     }
 }
